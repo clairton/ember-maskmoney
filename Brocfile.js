8096800dc1b73f819f45db7e678d8f088dd2cf50
Thiago <thiagobocorrea@gmail.com>
2015-04-07 19:52:28 -0300

    add import to index.js

--- Brocfile.js
+++ Brocfile.js
@@ -5,6 +5,4 @@ var EmberAddon = require('ember-cli/lib/broccoli/ember-addon');
 
 var app = new EmberAddon();
 
-app.import(app.bowerDirectory + '/jquery-maskmoney/dist/jquery.maskMoney.js');
-
 module.exports = app.toTree();
--- index.js
+++ index.js
@@ -2,5 +2,9 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-cli-maskmoney'
+  name: 'ember-cli-maskmoney',
+  included: function(app) {
+    this._super.included(app);
+    app.import(app.bowerDirectory + '/jquery-maskmoney/dist/jquery.maskMoney.js');
+  }
 };
