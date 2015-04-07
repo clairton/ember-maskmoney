0761084bbd02761234fb967c46ec40d45ad663d0
Thiago <thiagobocorrea@gmail.com>
2015-04-07 19:18:08 -0300

    extract from project

---
+++ addon/components/mask-money.js
@@ -0,0 +1,43 @@
+import Ember from 'ember';
+
+export default Ember.TextField.extend({
+  prefix: '',
+  suffix: '',
+  affixesStay: false,
+  thousands: ',',
+  decimal: '.',
+  precision: 2,
+  allowZero: true,
+  allowNegative: false,
+
+  options: function () {
+    return {
+      prefix: this.get('prefix'),
+      suffix: this.get('suffix'),
+      affixesStay: this.get('affixesStay'),
+      thousands: this.get('thousands'),
+      decimal: this.get('decimal'),
+      precision: this.get('precision'),
+      allowZero: this.get('allowZero'),
+      allowNegative: this.get('allowNegative')
+    };
+  }.property(),
+
+  initializeMask: function() {
+    this.$().maskMoney(this.get('options'));
+
+    if (this.get('number') !== undefined) {
+      var val = this.get('number').toString().replace(".", this.get('decimal'));
+      this.$().val(val);
+      this.$().maskMoney('mask');
+    }
+  }.on('didInsertElement'),
+
+  teardownMask: function() {
+    this.$().maskMoney('destroy');
+  }.on('willDestroyElement'),
+
+  setUnmaskedValue: function() {
+    this.set('number', this.$().maskMoney('unmasked')[0]);
+  }.observes('value')
+});
---
+++ app/components/mask-money.js
@@ -0,0 +1,3 @@
+import maskMoney from 'ember-cli-maskmoney/components/mask-money';
+
+export default maskMoney;
